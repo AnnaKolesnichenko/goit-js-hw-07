e8dcee5edcde026abe058a1e7672ef81f0ced24b
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-04-21 15:04:32 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -25,7 +25,7 @@ cardsGallery.insertAdjacentHTML('beforeend', cardsOfGallery);
 function onGalleryShow(e) {
     e.preventDefault();
     const lightbox = new SimpleLightbox('.gallery__item a', 
-     {captionDelay: 250, enableKeyboard: true, captionData: 'alt'});
+     {captionDelay: 250, enableKeyboard: true, captionData: 'alt', captions: true});
      lightbox.open();
 }
 
